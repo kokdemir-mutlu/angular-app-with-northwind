9523ba67b3d1d6444ee90789050f3e8f758cc5d9
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-08-21 18:38:53 +0300

    Taken the products from the product service and listed.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
+
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -8,13 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-
-
   products : Product[] = [];
+  dataLoaded : boolean = false;
 
-  constructor() { }
+  constructor(private productServie:ProductService,
+     private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      if(params['categoryId']){
+        this.getProductsByCategoryId(params['categoryId']);
+      }
+      else{
+        this.getProducts();
+      }
+    })
+  }
+
+  getProducts(){
+    this.productServie.getProducts().subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsByCategoryId(categoryId : number){
+    this.productServie.getProductsByCategoryId(categoryId).subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
   }
 
 }
