9e392d573a970b95361b61238ac6a6c44b209700
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-08-21 18:37:43 +0300

    Taken the categories from category service and listed in the category menu.

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,3 +1,5 @@
+import { CategoryService } from './../../services/category.service';
+import { Category } from './../../models/category';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categories : Category[] = [];
+  // currentCategory : Category = {
+  //   categoryId:0,
+  //   categoryName:''
+  // };// if strictPropertyInitializer is true
+
+  currentCategory : Category = {categoryId : 0, categoryName : ''};// if strictPropertyInitialization is false
+  constructor(private categoryService : CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response =>{
+      this.categories = response.data;
+    })
+  }
+
+  setCurrentCategory(category : Category){
+    console.log(category.categoryName);
+    this.currentCategory = category;
+  }
+
+  getCurrentCategory(category : Category){
+    if(category == this.currentCategory){
+      return 'list-group-item active';
+    }
+    else{
+      return 'list-group-item';
+    }
+  }
+
+  getClassForAllProducts(){
+    if(this.currentCategory.categoryId === 0){
+      return "list-group-item active"
+    }
+    else{
+      return 'list-group-item'
+    }
+  }
+
+  allProductsClicked(event : any){
+    this.currentCategory = {categoryId : 0, categoryName: ''};
   }
 
 }
